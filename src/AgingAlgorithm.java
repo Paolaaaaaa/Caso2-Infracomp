956556f46258e868d8f0492100b55accaba6e66b
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2022-10-15 11:41:51 -0500

    add agingAlgorithm template

---
+++ src/AgingAlgorithm.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class AgingAlgorithm implements Runnable {
+    ArrayList<Integer> nums;
+    int ram;
+    int tlb;
+
+    public int ageingAlgorithm(ArrayList<Integer> nums, int ram, int tlb) {
+        int ret = 0;
+
+        return ret;
+    }
+
+    public AgingAlgorithm(ArrayList<Integer> nums, int ram, int tlb) {
+        this.nums = nums;
+        this.ram = ram;
+        this.tlb = tlb;
+    }
+
+    @Override
+    public void run() {
+        ageingAlgorithm(this.nums, this.ram, this.tlb);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,16 @@ import java.util.ArrayList;
 
 public class Main {
     static FReader reader;
+    static AgingAlgorithm agingAlgorithm;
     public static void main(String[] args) {
+        int ram = 4;
+        int tlb = 4;
+
         reader = new FReader();
+
         ArrayList<Integer> nums = reader.readFile("examples/test_A2_R32_P8.txt");
+        agingAlgorithm = new AgingAlgorithm(nums,  ram,  tlb);
+
         System.out.println("Data recieved: "+ nums.toString());
     }
 }
